5555c892846181e802a6be9465d085eba79f2b7f
amritforu2019 <54579040+amritforu2019@users.noreply.github.com>
2022-08-23 13:58:23 +0530

    Commit 23082022

--- result-print.php
+++ result-print.php
@@ -18,7 +18,7 @@ require_once("con_base/functions.inc.php");
 <meta name="Rating" content="General" />
 <meta name="url" content="index.html">
 <meta name="Identifier-URL" content="index.html">
-<meta name="Reply-to" content="info@mhu.ac.in/" />
+<meta name="Reply-to" content="info@mpstu.in/" />
 <meta name="Owner" content="Maharana Pratap Horticultural University, Karnal" />
 <meta name="Author" content="iNet Business Hub, info@inetbusinesshub.com">
 <meta name="Designer" content="iNet Business Hub" />
@@ -277,9 +277,39 @@ $(function() {
 			FromLeft : true,
 			Overlay : true,
 			CollapseMenu : true,
-			ClassName : "mobile-menu"
+					ClassName : "mobile-menu"
 		});
-	});        
+	});
+
+    function includeHTML() {
+        var z, i, elmnt, file, xhttp;
+        /*loop through a collection of all HTML elements:*/
+        z = document.getElementsByTagName("*");
+        for (i = 0; i < z.length; i++) {
+            elmnt = z[i];
+            /*search for elements with a certain atrribute:*/
+            file = elmnt.getAttribute("w3-include-html");
+            if (file) {
+                /*make an HTTP request using the attribute value as the file name:*/
+                xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4) {
+                        if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                        if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                        /*remove the attribute, and call this function once more:*/
+                        elmnt.removeAttribute("w3-include-html");
+                        includeHTML();
+                    }
+                }
+                xhttp.open("GET", file, true);
+                xhttp.send();
+                /*exit the function:*/
+                return;
+            }
+        }
+    };
+
+        includeHTML();
 </script>
 </body>
 
